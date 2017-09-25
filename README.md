d16761e037c3f874b2b2d44218bb91dcb44f4321
j-vaira <31778358+j-vaira@users.noreply.github.com>
2017-09-25 17:20:25 -0600

    Finished.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ blank-repository-shell
 references:
 https://www.tutorialspoint.com/cplusplus/cpp_data_types.htm
 (I looked up the size of the types we're using so that I could better guess at what values to use to max them out)
+http://floating-point-gui.de/errors/rounding/
+(I wasn't sure exactly how to describe what was going on with the rounding errors other than "they're rounding errors" so I looked up an explanation.)
--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -2,6 +2,8 @@
 //Jasmine Vaira
 //2) We can detect overflow in this process by checking to see if the output of the function is negative. Since we are only summing positive integers, the output should
 //ordinarily never be negative. Same for 3).
+//4) We can detect overflow in the factorial process by checking to see if the output if infinity.
+//6) The value should be zero if computations are exact, but due to rounding errors it isn't always, and is instead a very, very small value.
 
 #include "stdafx.h"
 #include <iostream>
@@ -34,7 +36,43 @@ void sumLong(long n)
 void floatFactorial(long n)
 {
 	//compute the factorial of n integers
+	float factorial = 1;
+	for (long i = n; i > 0; i--)
+	{
+		factorial = factorial * i;
+	}
+	cout << n << "! is " << factorial << ".\n";
+}
 
+void doubleFactorial(long n)
+{
+	//compute the factorial of n integers
+	double factorial = 1;
+	for (long i = n; i > 0; i--)
+	{
+		factorial = factorial * i;
+	}
+	cout << n << "! is " << factorial << ".\n";
+}
+
+void floatRatio(float n)
+{
+	float sum = 0;
+	for (float i = n; i > 0; i--)
+	{
+		sum = sum + (1 / n);
+	}
+	cout << "For " << n << ", there is an error of " << sum - 1 << ".\n";
+}
+
+void doubleRatio(double n)
+{
+	double sum = 0;
+	for (double i = n; i > 0; i--)
+	{
+		sum = sum + (1 / n);
+	}
+	cout << "For " << n << ", there is an error of " << sum - 1 << ".\n";
 }
 
 int main()
@@ -52,6 +90,58 @@ int main()
 	sumLong(65535);
 	sumLong(65536); //this is where it's too big for type long. I figured that since the last one broke at 256 and it was 2 bytes in size, type long, which is 4 bytes, would break at 256 squared
 	sumLong(65537);
+
+	cout << endl;
+
+	//testing limits of float
+	floatFactorial(33);
+	floatFactorial(34);
+	floatFactorial(35); //this is where it overflows. I figured this out by running floatFactorial() through a loop. It's better than guessing.
+	floatFactorial(36);
+
+	cout << endl;
+
+	//testing limits of double
+	doubleFactorial(169);
+	doubleFactorial(170);
+	doubleFactorial(171); //overflow here. I ran this one through a loop like the last one to find the answer.
+	doubleFactorial(172);
+
+	cout << endl;
+
+	//testing rounding error for float
+	floatRatio(8);
+	floatRatio(9);
+	floatRatio(10); //this is where the rounding errors start. I ran it through a loop like the last one to find this. Some higher numbers do round right, most don't.
+	floatRatio(11);
+
+	cout << endl;
+
+	//testing rounding error for double
+	doubleRatio(4);
+	doubleRatio(5);
+	doubleRatio(6); //this is where the errors start. It's lower than for float, but that's because double holds more digits so has more potential for rounding error.
+	doubleRatio(7);
+
+	cout << endl;
+
+	//testing float error again
+	for (float i = 3.4; i < 4.4; i += 0.2)
+	{
+		cout << "i = " << i << endl; //the error occurs at the last pass through the loop. It should end at 4.2, but instead continues on and prints 4.4 as well. Because of the way float handles numbers, it rounds i down and thus runs through the loop one-too-many times.
+	}
+
+	cout << endl;
+	
+	//testing double error again
+	for (double i = 3.4; i < 4.4; i += 0.2)
+	{
+		cout << "i = " << i << endl; //double holds more, so doesn't round as many digits, and thus its accuracy is better in this case.
+	}
+
+
+	
+
 
 	//according to the guys on stackoverflow IDEs that don't automatically wait so you can see your output and close immediately after running are stupid
 	system("PAUSE");
